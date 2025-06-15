b9840a06761c25909e371aeb33389fa0cc408728
israel <User1@ULIK-DESKTOP1>
2025-06-15 14:26:01 +0300

    create basic flask server

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/test", methods=["GET"])
+def test():
+    print("hello")
+    return "Check console", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
